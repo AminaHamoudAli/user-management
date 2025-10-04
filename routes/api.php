1be53020f567f777848e8cb4502a7742188190f4
Amina Hamoud <amina123@tt.com>
2025-10-05 01:46:19 +0300

    feat(usecase): implement RegisterUser and EnableTwoFactor use cases

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/enable-2fa', [AuthController::class,'enable2fa']);
+    Route::post('/logout', [AuthController::class,'logout']);
+});
